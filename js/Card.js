4f8ad57176de1b5e59c8c54cf5cecd1157fedf8d
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-07-10 02:59:19 +0300

    Added setter for comment and changed html structure a little

--- js/Card.js
+++ js/Card.js
@@ -1,28 +1,48 @@
 class Card {
+  #commentEl;
   constructor(user, userId) {
     this.userId = userId;
-    this.commentEl = this.#buildCommentEl(user.comment);
+    this.#commentEl = this.#buildCommentEl();
     this.htmlEl = this.#buildHtml(user, userId);
+    this.comment = user.comment;
   }
 
-  #buildCommentEl(comment) {
-    let el = document.createElement("p");
-    el.classList.add("card__comment");
-    el.textContent = comment;
+  set comment(text) {
+    this.#commentEl.textContent = text ? text : "Комментарий отсутствует";
+  }
+
+  #buildCommentEl() {
+    let el = document.createElement("span");
+    el.classList.add("user-card__field-text");
     return el;
   }
 
   #buildHtml(user, userId) {
     let cardHtml = document.createElement("article");
-    cardHtml.classList.add("card");
-    cardHtml.innerHTML = `<h2 class="card__title">${userId}</h2>
-    <div class="card__info">
-      <p class="card__info-field"><span class="card__field-title">Login: </span>${user.login}</p>
-      <p class="card__info-field"><span class="card__field-title">Email: </span>${user.email}</p>
-      <p class="card__info-field"><span class="card__field-title">Animal: </span>${user.animal}</p>
-      <p class="card__info-field"><span class="card__field-title">Creation date: </span>${user.date}</p>
-    </div>`;
-    cardHtml.appendChild(this.commentEl);
+    cardHtml.classList.add("user-card", "card");
+    cardHtml.innerHTML = `<h3 class="user-card__title">${userId}</h3>
+    <div class="user-card__info">
+      <p class ="user-card__field">
+        <span class="user-card__field-title">Login: </span>
+        <span class="user-card__field-text">${user.login}</span>
+      </p>
+      <p class ="user-card__field">
+        <span class="user-card__field-title">Email: </span>
+        <span class="user-card__field-text">${user.email}</span>
+      </p>
+      <p class ="user-card__field">
+        <span class="user-card__field-title">Animal: </span>
+        <span class="user-card__field-text">${user.animal}</span>
+      </p>
+      <p class ="user-card__field">
+        <span class="user-card__field-title">Creation date: </span>
+        <span class="user-card__field-text">${user.date}</span>
+      </p>
+    </div>
+    <p class ="user-card__field user-card__field--comment">
+      <span class="user-card__field-title">Comment: </span>
+    </p>`;
+    cardHtml.querySelector(".user-card__field--comment").appendChild(this.#commentEl);
     return cardHtml;
   }
 }
