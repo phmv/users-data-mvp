147c0efd85f185182569ab64987b1c97e2702cc9
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-07-10 02:54:31 +0300

    Password and data properties added with descriptors, getters and setters to let iterate over them properly

--- js/User.js
+++ js/User.js
@@ -1,5 +1,26 @@
 class User {
   constructor(login, password, email, animal) {
+    Object.defineProperties(this, {
+      _password: {
+        writable: true,
+      },
+      _date: {
+        writable: true,
+      },
+      password: {
+        enumerable: true,
+        get: function () {
+          return this._password.split("").reverse().join("");
+        },
+      },
+      date: {
+        enumerable: true,
+        get: function () {
+          let formatter1 = new Intl.DateTimeFormat("ru");
+          return formatter1.format(this._date);
+        },
+      },
+    });
     this.login = login;
     this._password = password;
     this.email = email;
@@ -7,14 +28,5 @@ class User {
     this.number = Math.floor(Math.random() * (10000 - 1)) + 1;
     this._date = new Date();
     this.comment = null;
-  }
-
-  get password() {
-    return this._password.split("").reverse().join("");
-  }
-
-  get date() {
-    let formatter1 = new Intl.DateTimeFormat("ru");
-    return formatter1.format(this._date);
   }
 }
