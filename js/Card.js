dcbb11bfa9cd8e0d42f0d31e4547b07500a75705
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-07-10 17:32:20 +0300

    Styles for cards added

--- js/Card.js
+++ js/Card.js
@@ -13,35 +13,35 @@ class Card {
 
   #buildCommentEl() {
     let el = document.createElement("span");
-    el.classList.add("user-card__field-text");
+    el.classList.add("user-card__field-text", "d-block", "fst-italic", "text-muted");
     return el;
   }
 
   #buildHtml(user, userId) {
     let cardHtml = document.createElement("article");
     cardHtml.classList.add("user-card", "card");
-    cardHtml.innerHTML = `<h3 class="user-card__title">${userId}</h3>
-    <div class="user-card__info">
-      <p class ="user-card__field">
-        <span class="user-card__field-title">Login: </span>
+    cardHtml.innerHTML = `<h5 class="user-card__title card-header text-bg-primary">${userId}</h5>
+    <ul class="user-card__info list-group list-group-flush">
+      <li class ="user-card__field list-group-item">
+        <span class="user-card__field-title fw-bold">Login: </span>
         <span class="user-card__field-text">${user.login}</span>
-      </p>
-      <p class ="user-card__field">
-        <span class="user-card__field-title">Email: </span>
+      </li>
+      <li class ="user-card__field list-group-item">
+        <span class="user-card__field-title fw-bold">Email: </span>
         <span class="user-card__field-text">${user.email}</span>
-      </p>
-      <p class ="user-card__field">
-        <span class="user-card__field-title">Animal: </span>
+      </li>
+      <li class ="user-card__field list-group-item">
+        <span class="user-card__field-title fw-bold">Animal: </span>
         <span class="user-card__field-text">${user.animal}</span>
-      </p>
-      <p class ="user-card__field">
-        <span class="user-card__field-title">Creation date: </span>
+      </li>
+      <li class ="user-card__field list-group-item">
+        <span class="user-card__field-title fw-bold">Creation date: </span>
         <span class="user-card__field-text">${user.date}</span>
-      </p>
-    </div>
-    <p class ="user-card__field user-card__field--comment">
-      <span class="user-card__field-title">Comment: </span>
-    </p>`;
+      </li>
+    </ul>
+    <div class ="user-card__field user-card__field--comment card-body">
+      <span class="user-card__field-title card-text d-block fw-bold">Comment: </span>
+    </div>`;
     cardHtml.querySelector(".user-card__field--comment").appendChild(this.#commentEl);
     return cardHtml;
   }
